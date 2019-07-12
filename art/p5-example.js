2e9bc31830ea26001dcbead7279cd32669ddfcb1
Eric <erdavids@ncsu.edu>
2019-07-12 00:47:51 -0400

    successfully centered, trying to add to div

--- art/p5-example.js
+++ art/p5-example.js
@@ -1,8 +1,17 @@
+// function setup() {
+//   var cnv = createCanvas(400, 400);
+//   var x = (windowWidth - width) / 2;
+//   var y = (windowHeight - height) / 2;
+//   cnv.position(x, y);
+//   background(255, 0, 200);
+// }
+
 function setup() {
-  var cnv = createCanvas(400, 400);
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 2;
-  cnv.position(x, y);
+  var canvas = createCanvas(100, 100);
+
+  // Move the canvas so it’s inside our <div id="sketch-holder">.
+  canvas.parent('sketch-holder');
+
   background(255, 0, 200);
 }
 
