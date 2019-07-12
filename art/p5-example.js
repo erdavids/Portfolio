ead0234cc1e36848400afa336365eec262a5f743
Eric <erdavids@ncsu.edu>
2019-07-12 00:37:11 -0400

    Still can't figure it out

--- art/p5-example.js
+++ art/p5-example.js
@@ -1,19 +1,6 @@
-var cnv;
-
-function centerCanvas() {
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 2;
-  cnv.position(x, y);
-}
-
 function setup() {
-  cnv = createCanvas(100, 100);
-  centerCanvas();
+  createCanvas(windowWidth, windowHeight);
   background(255, 0, 200);
-}
-
-function windowResized() {
-  centerCanvas();
 }
 
 function draw() {
