ac2d50e869541681f4295586aee87c1acdcd5ef4
Eric <erdavids@ncsu.edu>
2019-07-24 16:50:07 -0400

    Trying out new way of drawing pieces

--- projects/chess-engine/chess-engine.js
+++ projects/chess-engine/chess-engine.js
@@ -87,7 +87,7 @@ function draw_empty_board() {
 
 function draw_pieces() {
   for (let x = 0; x < 8; x++) {
-    for (let x = 0; x < 8; x++) {
+    for (let y = 0; y < 8; x++) {
       if (board[x][y] == 1)
         draw_piece_on_grid(bP, x, y);
     }
