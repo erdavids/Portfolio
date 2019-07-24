c7f9b7bdd2fba3928127b1f053e3ee7aa14ae45a
Eric <erdavids@ncsu.edu>
2019-07-24 14:48:08 -0400

    Getting null for some reason

--- projects/chess-engine/chess-engine.js
+++ projects/chess-engine/chess-engine.js
@@ -19,6 +19,7 @@ function preload() {
 }
 
 function grid_to_pixel(x, y) {
+  console.log(x)
   return [x*square_size, y*square_size];
 }
 
@@ -26,6 +27,7 @@ function draw_piece_on_grid(piece, x, y) {
   var pix = grid_to_pixel(x, y);
   console.log(pix);
   image(piece, pix[0], pix[1], square_size, square_size);
+
 }
 
 function setup() {
