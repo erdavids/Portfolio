adcc42583dce49bc3abf19b3ee501eb08bb548a9
Eric <erdavids@ncsu.edu>
2019-07-25 18:13:17 -0400

    Took a risk to add ownwards movement as well

--- projects/chess-engine/chess-engine.js
+++ projects/chess-engine/chess-engine.js
@@ -268,6 +268,16 @@ function get_moves(p, grid_click) {
       el += 1;
     }
     // Down
+    ri = 1
+    while (r + ri <= 7 && board[r + ri][c] == 0) {
+      m[el] = [r + ri, c];
+      el += 1;
+      ri += 1;
+    }
+    if (r + ri >= 0 && board[r + ri][c] < 7 && board[r + ri][c] > 0) {
+      m[el] = [r + ri, c];
+      el += 1;
+    }
   }
 
   return m;
