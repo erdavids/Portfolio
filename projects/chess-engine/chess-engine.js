9b7aa6b4debd0f8195ae1e8cb7e8b63712d7dafc
Eric <erdavids@ncsu.edu>
2019-07-26 16:59:28 -0400

    Made a potentially important fix

--- projects/chess-engine/chess-engine.js
+++ projects/chess-engine/chess-engine.js
@@ -188,7 +188,7 @@ function mini_max(depth, b, alpha, beta, is_max) {
           temp_board[black_moves[i][0]][black_moves[i][1]] = temp_board[r][c]
           temp_board[r][c] = 0
 
-          best_move = mini_max(depth - 1, temp_board, alpha, beta, !is_max);
+          best_move = Math.max(best_move, mini_max(depth - 1, temp_board, alpha, beta, !is_max));
           alpha = Math.max(alpha, best_move);
           if (alpha >= beta) {
             return best_move;
@@ -208,7 +208,7 @@ function mini_max(depth, b, alpha, beta, is_max) {
           temp_board[white_moves[i][0]][white_moves[i][1]] = temp_board[r][c]
           temp_board[r][c] = 0
 
-          best_move = mini_max(depth - 1, temp_board, alpha, beta, !is_max);
+          best_move = Math.min(best_move, mini_max(depth - 1, temp_board, alpha, beta, !is_max));
           beta = Math.min(beta, best_move);
           if (alpha >= beta) {
             return best_move;
