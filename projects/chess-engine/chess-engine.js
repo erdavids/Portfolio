97f8105ae8611f9e495163b1d1f2e52816458dc4
Eric <erdavids@ncsu.edu>
2019-07-26 17:04:34 -0400

    Also printing all temp boards at the top level

--- projects/chess-engine/chess-engine.js
+++ projects/chess-engine/chess-engine.js
@@ -152,6 +152,8 @@ function mini_max_root(depth) {
         temp_board[black_moves[i][0]][black_moves[i][1]] = temp_board[r][c]
         temp_board[r][c] = 0
 
+        console.log(temp_board)
+
         value = mini_max(depth - 1, temp_board, -100000, 100000, false);
 
         if (value > best_move) {
