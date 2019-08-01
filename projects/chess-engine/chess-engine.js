a3da56a5c31b2aa4b61bd67c28aa657ede867d3a
Eric <erdavids@ncsu.edu>
2019-08-01 14:27:33 -0400

    Added a cheating move

--- projects/chess-engine/chess-engine.js
+++ projects/chess-engine/chess-engine.js
@@ -1192,9 +1192,15 @@ function get_black_moves(b, p, r, c) {
     }
 
     // Cheating moves
-    if (crooked == true && root_cheat == true && random(1) < .05) {
+    if (crooked == true && root_cheat == true && random(1) < .2) {
       // Straight up
       if (r > 1 && (b[r - 2][c] > 6 || b[r - 2][c] == 0)) {
+        m[el] = [r - 2, c];
+        el += 1;
+      }
+
+      // Straight Down
+      if (r < 7 && (b[r + 2][c] > 6 || b[r + 2][c] == 0)) {
         m[el] = [r - 2, c];
         el += 1;
       }
