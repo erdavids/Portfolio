7e146b1f8c29ceaa8e4c217d4eb6d0a570e90b8a
Eric <erdavids@ncsu.edu>
2020-04-30 14:16:02 -0400

    Added new dat.gui categories

--- art/generative-plants/generative-plants.js
+++ art/generative-plants/generative-plants.js
@@ -2,48 +2,88 @@ p5.disableFriendlyErrors = true;
 
 const opts = {
   // Generation Details
-  iterations: 5,
-  length: 300,
-  ang: 25,
+  Iterations: 5,
+  Length: 300,
+  Angle: 25,
+  Angle_Drift: 5,
+  rule0a: 'F',
+  rule0b: 'Go[-F]S[+G+F*][+F]S[-G-F*]',
+  rule1a: 'G',
+  rule1b: 'G[+F]G[-F]S',
+  Background: [211, 206, 194],
+  Shape: [172, 187, 173],
+  Fruit: [200, 140, 100],
+  Red_Drift: 20,
+  Green_Drift: 20,
+  Blue_Drift: 20,
   
   // Additional Functions
-  randomize: () => randomize(),
-  save: () => save(),
+  Generate: () => randomize(),
+  Save: () => save(),
   create: () => createPlant(),
 };
 
 window.onload = function() {
   var gui = new dat.GUI({width:300});
   // gui.remember(opts)
-  gui.add(opts, 'iterations', 1, 7).step(1).onChange(setup);
-  gui.add(opts, 'randomize')
-  gui.add(opts, 'save');
+  gui.add(opts, 'Iterations', 1, 7).step(1).onChange(setup);
+  gui.add(opts, 'Length').onChange(updateRules);
+  gui.add(opts, 'Angle').onChange(updateRules);
+  gui.add(opts, 'Angle_Drift').onChange(updateRules);
+  gui.add(opts, 'rule0a').onChange(updateRules);
+  gui.add(opts, 'rule0b').onChange(updateRules);
+  gui.add(opts, 'rule1a').onChange(updateRules);
+  gui.add(opts, 'rule1b').onChange(updateRules);
+  gui.addColor(opts, 'Background')
+  gui.addColor(opts, 'Shape')
+  gui.addColor(opts, 'Fruit')
+  gui.add(opts, 'Red_Drift');
+  gui.add(opts, 'Green_Drift');
+  gui.add(opts, 'Blue_Drift');
+  gui.add(opts, 'Generate');
+  gui.add(opts, 'Save');
+                               
 
 };
 
-var angle;
+var ang;
 var axiom = "F"
 var sentence = axiom
-var len = opts.length;
+var len = opts.Length;
 
 var rules = []
 rules[0] = {
-    a: 'F',
-    b: 'Go[-F]S[+G+F*][+F]S[-G-F*]'
+    a: opts.rule0a,
+    b: opts.rule0b
 };
 
 rules[1] = {
-    a: 'G',
-    b: 'G[+F]G[-F]S'
+    a: opts.rule1a,
+    b: opts.rule1b,
 };
 
 function randomize() {
-  noiseSeed()
+  randomSeed(random(10000))
+  updateRules()
   setup()
 }
 
 function save() {
   save('photo.png');
+}
+
+function updateRules() {
+  rules[0] = {
+    a: opts.rule0a,
+    b: opts.rule0b
+  };
+  
+  rules[1] = {
+      a: opts.rule1a,
+      b: opts.rule1b,
+  };
+  
+  len = opts.Length
 }
 
 function generate(iter) {
@@ -74,9 +114,9 @@ function generate(iter) {
 
 function turtle(iter) {
   
-  background(211, 206, 194);
+  background(opts.Background);
   
-  fill(172, 187, 173, 200)
+  fill(opts.Shape)
   noStroke()
   // pushMatrix()
   translate(width/2, height/2)
@@ -96,11 +136,11 @@ function turtle(iter) {
       line(0, 0, 0, -len)
       translate(0, -len);
     } else if (current == '+') {
-      angle = radians(random(10, 30))
-      rotate(angle);
+      ang = radians(random(opts.Angle - opts.Angle_Drift, opts.Angle + opts.Angle_Drift))
+      rotate(ang);
     } else if (current == '-') {
-      angle = radians(random(10, 30))
-      rotate(-angle);
+      ang = radians(random(opts.Angle - opts.Angle_Drift, opts.Angle + opts.Angle_Drift))
+      rotate(-ang);
     } else if (current == '[') {
       push();
     } else if (current == ']') {
@@ -111,7 +151,7 @@ function turtle(iter) {
       noFill()
       circle_calls += 1
       if (circle_calls > 50 && random(1) < .4) {
-        fill(random(180, 210), random(100, 140), 100, random(255, 255))
+        fill(opts.Fruit[0] + random(-opts.Red_Drift, opts.Red_Drift), opts.Fruit[1] + random(-opts.Green_Drift, opts.Green_Drift), opts.Fruit[2] + random(-opts.Blue_Drift, opts.Blue_Drift))
         circle(0, 0, random(4, 13))
       }
     }
@@ -128,22 +168,24 @@ function setup()
 
   pixelDensity(2);
   
-  angle = radians(opts.angle);
+  ang = radians(opts.Angle);
+  len = opts.Length
   
   var cnv = createCanvas(width, height);
   cnv.parent('sketchdiv');
+ 
+  
   
   strokeWeight(1);
   background(211, 206, 194);
   
-  len = opts.length
   sentence = axiom
   createPlant();
   
 }
 
 function createPlant() {
-  for (var i = 0; i < int(opts.iterations); i++) {
+  for (var i = 0; i < int(opts.Iterations); i++) {
     generate(i);
   }
 }
