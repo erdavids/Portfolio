735d9247ea5957cb876e96011a2fcb67ce313225
erdavids <erdavids@ncsu.edu>
2021-01-12 09:34:59 -0500

    Changed to Tweakpane

--- proof_of_concept.js
+++ proof_of_concept.js
@@ -13,6 +13,11 @@ const opts = {
     noise_multiplier: 100,
     noise_dampener: 1,
     image_border_buff: 15,
+    x_offset: 0,
+    y_offset: 0,
+  
+    background: '#262833',
+    cube: '#ffffff',
     
     // // Additional Functions
     GitHub: () => github(),
@@ -22,21 +27,68 @@ const opts = {
     create: () => createPlant(),
   };
   
-  window.onload = function() {
-    var gui = new dat.GUI({width:300});
-    gui.add(opts, 'grid_height', 3, 200).step(1)
-    gui.add(opts, 'grid_width', 3, 200).step(1)
-    gui.add(opts, 'block_size', 2, 30).step(1)
-    gui.add(opts, 'block_height', 1, 6).step(1)
-    gui.add(opts, 'lines', 1, 4).step(1)
-    gui.add(opts, 'Generate');
-    gui.add(opts, 'Save');
-    var made = gui.addFolder('Made by Eric Davidson')
-    made.add(opts, 'GitHub')
-    made.add(opts, 'YouTube')
-                                 
-  
-  };
+
+
+const pane = new Tweakpane();
+
+pane.addInput(opts, 'grid_height', {
+  min: 1,
+  max: 100,
+  step: 1
+});
+
+pane.addInput(opts, 'grid_width', {
+  min: 1,
+  max: 100,
+  step: 1
+});
+
+
+pane.addInput(opts, 'block_size', {
+  min: 1,
+  max: 30,
+  step: 1
+});
+
+
+pane.addInput(opts, 'block_height', {
+  min: 1,
+  max: 15,
+  step: 1
+});
+
+pane.addInput(opts, 'y_offset', {
+  min: -300,
+  max: 300,
+  step: 10
+});
+
+pane.addInput(opts, 'x_offset', {
+  min: -600,
+  max: 600,
+  step: 10
+});
+
+
+
+pane.addInput(opts, 'background');
+pane.addInput(opts, 'cube');
+
+
+const generate_btn = pane.addButton({
+  title: 'Generate New Image',
+});
+generate_btn.on('click', () => {
+  randomize()
+});
+
+const save_btn = pane.addButton({
+  title: 'Download',
+});
+save_btn.on('click', () => {
+  save()
+});
+
   
   function github() {
       window.open('https://github.com/erdavids/Portfolio/tree/master/road-lattice')
@@ -47,7 +99,7 @@ const opts = {
 }
 
   function randomize() {
-    setup();
+    draw();
   }
   
   function save() {
@@ -86,36 +138,48 @@ function draw_block(x, y, h) {
 }
 
 function setup(){
+    noLoop();
+ 
+}
+
+function draw() {
   
       noiseSeed(random(0, 10000));
       var canvasDiv = document.getElementById('sketchdiv');
 
     var iterations = 1
-      var w = opts.grid_width * opts.block_size + opts.grid_height * opts.block_size + opts.image_border_buff * opts.block_size
-    var h = opts.grid_height * opts.block_size/2 + opts.grid_width * opts.block_size/2 + (parseInt(noise(0, 0) * opts.noise_multiplier) / opts.noise_dampener * opts.block_height) + opts.image_border_buff * opts.block_size
+    
+    var myDiv = document.getElementById("grab-me");
+    var myWidth = myDiv.clientWidth;
+      var w = myWidth
+      // var w = opts.grid_width * opts.block_size + opts.grid_height * opts.block_size + opts.image_border_buff * opts.block_size
+      var h = myDiv.clientHeight;
+    // var h = opts.grid_height * opts.block_size/2 + opts.grid_width * opts.block_size/2 + (parseInt(noise(0, 0) * opts.noise_multiplier) / opts.noise_dampener * opts.block_height) + opts.image_border_buff * opts.block_size
 
     var start_block_x = w/2 - opts.grid_height/2 * opts.block_size + opts.grid_width/2 * opts.block_size
     var start_block_y = h/2 - opts.grid_height/2 * opts.block_size/2 - opts.grid_width/2 * opts.block_size/2 + (parseInt(noise(0, 0) * opts.noise_multiplier) / opts.noise_dampener * opts.block_height/2)
 
-    var cnv = createCanvas(w, h);
+    var cnv = createCanvas(myWidth, h);
     cnv.parent('sketchdiv');
-    background(255)
   
-    pixelDensity(1)
+  
+    pixelDensity(2)
     
     strokeWeight(opts.sw)
+  
+    background(opts.background)
+  
+    fill(opts.cube)
 
-    for (let g = 0; g < iterations; g++) {
-        
       for (let x = 0; x < opts.grid_height; x++) {
             for (let y = 0; y < opts.grid_width; y++) {
                 
-                cubes = int(noise((x + g) * opts.noise_scale, y * opts.noise_scale) * opts.noise_multiplier) / opts.noise_dampener
+                cubes = int(noise((x) * opts.noise_scale, y * opts.noise_scale) * opts.noise_multiplier) / opts.noise_dampener
                 
               for (let i = parseInt(cubes) - 1; i < parseInt(cubes); i++) {
-                    draw_block((start_block_x + x*opts.block_size) - y*opts.block_size, (start_block_y + x*(opts.block_size/2)) + y*(opts.block_size/2) - int(i)*(opts.block_height), i)
+                    draw_block((start_block_x + x*opts.block_size) - y*opts.block_size + opts.x_offset, (start_block_y + x*(opts.block_size/2)) + y*(opts.block_size/2) - int(i)*(opts.block_height) + opts.y_offset, i)
             }
-      }
+      
       }
     }
 }
