058cca01f147f75b889ded1f851987d09e4a99d1
erdavids <erdavids@ncsu.edu>
2021-04-04 10:43:02 -0400

    Adding a lot to the styling of the blog, wrapping up the small p5 sketch for now

--- generative-plants.js
+++ generative-plants.js
@@ -3,11 +3,11 @@ p5.disableFriendlyErrors = true;
 const opts = {
   // Generation Details
   Width: 300,
-  Height: 100,
-  OriginalPoints: [[20, 20], [40, 20], [20, 40]],
+  Height: 70,
+  OriginalPoints: [[300/2 - 40, 100/2 - 40], [300/2 + 40, 100/2 - 40], [300/2 + 60, 100/2 - 20]],
   Points: [],
 
-  maxDistance: 10,
+  maxDistance: 30,
 };
 
 function setup()
@@ -22,10 +22,29 @@ function setup()
   cnv.parent('sketchdiv');
 
   background('#54565D');
-  stroke(255)
+  stroke('#eedfc8')
+  strokeWeight(2)
+  fill('#D9A06A')
 
   // Deep Copy
-  opts.Points = JSON.parse(JSON.stringify(opts.OriginalPoints));
+  // opts.Points = JSON.parse(JSON.stringify(opts.OriginalPoints));
+
+  opts.Points = [[opts.Width/2 - 75, opts.Height/2], [opts.Width/2 - 25, opts.Height/2], [opts.Width/2 + 25, opts.Height/2], [opts.Width/2 + 75, opts.Height/2]]
+
+  for (var p in opts.Points) {
+    let c = opts.Points[p]
+    fill('#D9A06A')
+    circle(c[0], c[1], 30)
+    noFill()
+    stroke('#eedfc8')
+    strokeWeight(2)
+    
+    let movement_x = getRandomArbitrary(-15, 15)
+    let movement_y = getRandomArbitrary(-15, 15)
+
+    circle(c[0] + movement_x, c[1] + movement_y, 30)
+
+  }
 
 }
 
@@ -39,8 +58,8 @@ function getDistance(p1, p2) {
 
 function deformPoints() {
   for (var p in opts.Points) {
-    let movement_x = getRandomArbitrary(-.1, .2)
-    let movement_y = getRandomArbitrary(-.1, .2)
+    let movement_x = getRandomArbitrary(-.2, .3)
+    let movement_y = getRandomArbitrary(-.2, .3)
 
     if (getDistance([opts.Points[p][0] + movement_x, opts.Points[p][1] + movement_y], opts.OriginalPoints[p]) < opts.maxDistance) {
       opts.Points[p][0] += movement_x 
@@ -50,15 +69,15 @@ function deformPoints() {
   }
 }
 
-function draw() {
-  background('#54565D');
-  beginShape()
-  for (var p in opts.Points) {
-    let c = opts.Points[p]
-    vertex(c[0], c[1])
-  }
-  endShape(CLOSE)
+// function draw() {
+//   background('#54565D');
+//   beginShape()
+//   for (var p in opts.Points) {
+//     let c = opts.Points[p]
+//     vertex(c[0], c[1])
+//   }
+//   endShape(CLOSE)
 
-  deformPoints()
+//   deformPoints()
 
-}
+// }
